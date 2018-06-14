d63b8ea05d1055a7a38db1c7f01a9478d086d8dc
Lachlan Heywood <lachlan@morrisonagency.com>
2018-06-13 20:28:50 -0400

    add about router to app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 const commentsRouter = require('./routes/comments');
+const aboutRouter = require('./routes/about');
 
 const app = express();
 dotenv.load();
@@ -32,6 +33,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/comments', commentsRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
